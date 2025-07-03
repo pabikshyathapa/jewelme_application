27fb5de9e28353a3b3d51df244552099514d5d2b
pabikshyathapa <pabikshyathapa@gmail.com>
2025-07-03 17:41:21 +0545

    api update

--- lib/features/auth/presentation/view_model/login_view_model/login_view_model.dart
+++ lib/features/auth/presentation/view_model/login_view_model/login_view_model.dart
@@ -63,11 +63,15 @@ class LoginViewModel extends Bloc<LoginEvent, LoginState> {
           message: "Login Successful",
           color: Colors.green,
         );
-       
+        Navigator.pushReplacement(
+          event.context,
+          MaterialPageRoute(
+            builder: (_) => DashboardPage(),
+          ),
+        );
         // Optional: Navigate to Home after login
         // add(NavigateToHomeViewEvent(context: event.context));
       },
     );
   }
-
 }
